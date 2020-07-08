1d83ae8066a1cac079faa3ebbbf15de735a29a76
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-07-08 16:18:00 -0500

    word_count complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,23 @@
-def word_count(s):
-    # Your code here
+import re
 
+
+def word_count(text):
+    word_count = {}
+    word_list = text.split()
+    for word in word_list:
+        word = re.sub(r'[^\w\']', '', word)
+        word = word.lower()
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            if word != '':
+                word_count[word] = 1
+    return word_count
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
