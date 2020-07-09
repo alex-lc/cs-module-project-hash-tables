e0e6e2dc959e8734537ba109ba2dd8787586d27f
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-07-09 16:01:06 -0500

    markov complete

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -11,14 +11,44 @@ def gobbledegook(text):
     words_list = text.split()
     cache = {}
 
-    for word in range(len(words_list)):
-        if words_list[word] not in cache:
-            cache[words_list[word]] = [words_list[word], words_list[word + 1]]
-    
-    print(cache)
+    for i in range(len(words_list)-1):
+        if words_list[i] in cache:
+            cache[words_list[i]].append(words_list[i+1])
+        else:
+            cache[words_list[i]] = [words_list[i+1]]
+    cache[words_list[-1]] = []
+    return cache
+
+
+sentence = "Cats and dogs and birds and fish dogs birds?"
+# print(gobbledegook(sentence))
 
 # TODO: construct 5 random sentences
-# Your code here
 
 
-gobbledegook(words)
+def make_random_sentences(text):
+    cache = gobbledegook(text)
+    random_word = random.choice(text.split())
+
+    stop_conditions = ['.', '?', '!', '."', '?"', '!"']
+
+    current_words = cache[random_word]
+    curr = random.choice(current_words)
+    sentence = random_word
+
+    while curr[-1] not in stop_conditions and curr[-2:] + curr[-1] not in stop_conditions and len(cache[curr]) > 1:
+        if cache[curr] != [] and cache[curr] != None:
+            sentence = sentence + " " + curr
+            curr = random.choice(cache[curr])
+        else:
+            curr = random.choice(current_words)
+
+    sentence += " " + curr
+    print(sentence)
+
+
+make_random_sentences(words)
+make_random_sentences(words)
+make_random_sentences(words)
+make_random_sentences(words)
+make_random_sentences(words)
